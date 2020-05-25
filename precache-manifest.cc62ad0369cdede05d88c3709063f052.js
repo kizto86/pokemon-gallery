021eafe94de90198c55bdf3be4d93a7ee4d601b7
kizto86 <kizto86@gmail.com>
2020-05-25 08:35:54 +0200

    Updates

---
+++ precache-manifest.cc62ad0369cdede05d88c3709063f052.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2f3e96f392719619d1c75817c59a6f",
+    "url": "/React_Pokemon/index.html"
+  },
+  {
+    "revision": "219d25e987db3f02c20a",
+    "url": "/React_Pokemon/static/css/main.b90b9bfe.chunk.css"
+  },
+  {
+    "revision": "7448b27bcd48360550ac",
+    "url": "/React_Pokemon/static/js/2.b6d96ff8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Pokemon/static/js/2.b6d96ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "219d25e987db3f02c20a",
+    "url": "/React_Pokemon/static/js/main.d4664e79.chunk.js"
+  },
+  {
+    "revision": "7261f5d78a8112703671",
+    "url": "/React_Pokemon/static/js/runtime-main.3514c663.js"
+  }
+]);
