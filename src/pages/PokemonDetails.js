1553d775c69a3a2dcb6495ec6c9e5736788aaf3b
kizto86 <kizto86@gmail.com>
2020-05-26 23:52:29 +0200

    detail_page_made

---
+++ src/pages/PokemonDetails.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Pokemons from "../Page/Pokemons";
+
+const PokemonDetail = () => {
+  return <div>PokemonDetail</div>;
+};
+
+export default PokemonDetail;
---
+++ src/pages/Pokemons.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+//import "./App.css";
+
+//import Header from "../components/Header";
+import axios from "axios";
+import { Multiselect } from "multiselect-react-dropdown";
+import PokemanList from "../components/PokemonList";
+
+class Pokemons extends Component {
+  pokemonDetails = [];
+
+  constructor() {
+    super();
+    this.state = {
+      pokemons: [],
+      pokemonType: [],
+      displayList: [],
+    };
+  }
+  //SETTING ALL DATA FETCHING INSIDE REACT ComponentDidMount LIFECYCLE
+
+  componentDidMount() {
+    this.getPokemon();
+    this.getPoketype();
+  }
+
+  //FETCHES THE DIFFERENT TYPES OF POKEMON
+
+  getPoketype() {
+    axios
+      .get("https://pokeapi.co/api/v2/type")
+      .then((response) => {
+        this.setState({ pokemonType: response.data.results });
+      })
+      .catch((error) => {
+        console.log("Error fetching and parsing data", error);
+      });
+  }
+
+  //FETCHES THE FIRST 150 POKEMON
+
+  getPokemon() {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=150")
+      .then((response) => {
+        this.setState({ pokemons: response.data.results });
+
+        // eslint-disable-next-line array-callback-return
+        this.state.pokemons.map((pokemon) => {
+          axios
+            .get(pokemon.url)
+            .then((response) => {
+              var temp = this.pokemonDetails;
+              temp.push(response.data);
+              let result = temp.slice(0);
+              result.sort((a, b) => {
+                return a.id - b.id;
+              });
+              this.setState({ displayList: result });
+            })
+            .catch((error) => {
+              console.log("Error fetching and parsing data", error);
+            });
+        });
+      })
+      .catch((error) => {
+        console.log("Error fetching and parsing data", error);
+      });
+  }
+
+  //HANDLES THE OnRemove EVENT
+
+  onSelect = (selectedList) => {
+    this._filterFunction(selectedList);
+  };
+
+  //HANDLES THE onRemove EVENT
+
+  onRemove = (selectedList) => {
+    if (selectedList.length > 0) {
+      this._filterFunction(selectedList);
+    } else {
+      this.setState({ displayList: this.pokemonDetails });
+    }
+  };
+
+  //THIS FUNCTION  HANDLES THE FILTERING LOGIC
+
+  _filterFunction = (selectedList) => {
+    let filteredList = [];
+    filteredList = this.pokemonDetails.filter((pokemon) => {
+      return selectedList.some((f) => {
+        return pokemon.types.some((pkType) => {
+          return f.name === pkType.type.name;
+        });
+      });
+    });
+
+    this.setState({ displayList: filteredList });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        {/*<Header />*/}
+
+        <Multiselect
+          options={this.state.pokemonType}
+          selectedValues={this.state.selectedValue}
+          onSelect={this.onSelect}
+          onRemove={this.onRemove}
+          displayValue="name"
+        />
+        <br />
+
+        <PokemanList data={this.state.displayList} />
+      </div>
+    );
+  }
+}
+
+export default Pokemons;
